74730a4e1875708c85c09c10e543cd8f6acff99b
quborg <miloud.laftah@gmail.com>
2018-05-22 02:05:42 +0000

    image optimization with ImageCompressor.js (png|jpg|jpeg)

--- client/src/react/components/Media/ImageFileLoader.js
+++ client/src/react/components/Media/ImageFileLoader.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import {Input} from 'reactstrap'
 import {Image, PREVIEW_IMG_SRC} from 'ayla-client/react/components/Media'
+import ImageCompressor from 'image-compressor.js';
+
+const options = {
+  quality: .6,
+  maxWidth: 200,
+  maxHeight: 200
+}
 
 
 export default class ImageFileLoader extends Component {
@@ -16,16 +23,31 @@ export default class ImageFileLoader extends Component {
     let file    = e.target.files ? e.target.files[0] : null
       , reader  = new FileReader()
       , image   = ''
+      , self    = this
       , {keyName} = this.props
 
     if (file) {
       if (file.type.match('image\/(jpg|png|jpeg)')) {
-        reader.readAsDataURL(file)
-        reader.onload = e => {
-          image = { src: reader.result, name: file.name }
-          this.props.handler({ [keyName]: image })
-          this.props.progress(100)
-        }
+        new ImageCompressor(file, {
+          ...options,
+          beforeDraw: (ctx, canvas) => {
+            // canvas.width = 200
+            // canvas.height = 200
+          },
+          success: result => {
+            reader.readAsDataURL(result)
+            reader.onload = e => {
+              image = { src: reader.result, name: file.name }
+              this.props.handler({ [keyName]: image })
+              this.props.progress(100)
+            }
+            console.log('Upload success', result)
+          },
+          error: e => {
+            console.log('ERROR ImageCompressor() :', err)
+            this.props.handler({ [keyName]: 'error-file-type' })
+          },
+        });
       }
       else { this.props.handler({ [keyName]: 'error-file-type' }) }
     } else {
